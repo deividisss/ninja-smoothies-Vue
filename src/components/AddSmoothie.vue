523b578ee664204125313e636ea5e1f5d16e5f17
Deividas <deivis124@gmail.com>
2020-07-15 02:00:32 +0300

    Adds ability to add ingredients to the smooothie and save smoothie to the firebase

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -6,11 +6,16 @@
         <label for="title">Smoothie Title:</label>
         <input type="text" name="title" v-model="title" id />
       </div>
+      <div v-for="(ing, index) in ingredients" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]" />
+      </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient</label>
-        <input type="text" name="add-ingredient" />
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another" />
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -18,16 +23,55 @@
 </template>
 
 <script>
+import db from "@/firebase/init";
+import slugify from "slugify";
+
 export default {
   name: "AddSmoothie",
   data() {
     return {
-      tite: null
+      tite: null,
+      another: null,
+      ingredients: [],
+      feedback: null,
+      slug: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title);
+      if (this.title) {
+        this.feedback = null;
+        // create slug
+        this.slug = slugify(this.title, {
+          replacement: "-",
+          remove: /[$*_+~.(),'"!\-:@]/g,
+          lower: true
+        });
+        console.log(this.slug);
+        db.collection("smoothies")
+          .add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug
+          })
+          .then(() => {
+            this.$router.push({ name: "Index" });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        this.feedback = "You must enter a smoothie title";
+      }
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value to add an ingredient";
+      }
     }
   }
 };
